022a11ebdaa1983dc260859da80a44f01aad5c10
admackenzie <admacdev@pm.me>
2024-03-04 14:10:47 -0500

    Improve MobilNav visibility

--- app/ui/MobileNav.tsx
+++ app/ui/MobileNav.tsx
@@ -43,7 +43,19 @@ export default function MobileNav({ bookmarks, removeBookmark }: Props) {
 	const handleScroll = debounce(() => {
 		const currentScrollPos = window.scrollY;
 
-		currentScrollPos < prevScrollPos ? setVisible(false) : setVisible(true);
+		// Show nav at this distance in pixels from the top or bottom
+		const threshold = 300;
+
+		const maxY =
+			document.body.scrollHeight - threshold <=
+			currentScrollPos + window.innerHeight;
+
+		const minY = currentScrollPos <= threshold;
+
+		// Show nav when the user scrolls down and when the user is at the top or the bottom of the content
+		currentScrollPos > prevScrollPos || maxY || minY
+			? setVisible(true)
+			: setVisible(false);
 
 		setPrevScrollPos(currentScrollPos);
 	}, 50);
